2ca9f0c4fa85b211cb72300f2961bde60a4a3f18
iasonasxrist <80000902+iasonasxrist@users.noreply.github.com>
2023-09-09 18:20:43 +0300

    Initialization

---
+++ app.js
@@ -0,0 +1,35 @@
+// SHOW MENU
+
+
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'), 
+        navClose = document.getElementById('nav-close')
+
+        // Show menu
+// if (navToggle) {
+    // addEventListener
+    // classList add Menu
+// }
+
+
+  // Hidden menu
+
+
+// if (navClose) {
+    // addEventListener
+    // classList remove Menu
+// }
+
+
+
+// Remove Menu Mobile
+
+const navLink = document.querySelectorAll('nav__link')
+
+// const linkAction = () => {
+    // Select nav-menu 
+    // remove('show-menu')
+
+// }
+
+// navLink.forEach(n=> n.addEventListener('click,l inkAction ))
