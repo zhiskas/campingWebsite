1650fb5d7d34a3418c1b461ba426e22d70f62912
iasonasxrist <80000902+iasonasxrist@users.noreply.github.com>
2023-10-07 17:13:33 +0300

    Camp-Website

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
 // SHOW MENU
 
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-const navMenu = document.getElementById('nav-menu'),
-        navToggle = document.getElementById('nav-toggle'), 
-        navClose = document.getElementById('nav-close')
+// Show menu
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
-        // Show menu
-// if (navToggle) {
-    // addEventListener
-    // classList add Menu
-// }
+// Hidden menu
 
-
-  // Hidden menu
-
-
-// if (navClose) {
-    // addEventListener
-    // classList remove Menu
-// }
-
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 // Remove Menu Mobile
 
-const navLink = document.querySelectorAll('nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-// const linkAction = () => {
-    // Select nav-menu 
-    // remove('show-menu')
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // Select nav-menu and remove show-menu on click
+  navMenu.classList.remove("show-menu");
+};
 
-// }
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-// navLink.forEach(n=> n.addEventListener('click,l inkAction ))
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+//
+
+gsap.from(".home__img-2", 1.2, { opacity: 0, y: 200, delay: 0.1 });
+gsap.from(".home__img-3", 1.2, { opacity: 0, y: 200, delay: 0.5 });
+gsap.from(".home__data", 1.2, { opacity: 0, y: -60, delay: 0.5 });
+gsap.from(".home__bird-1", 1.2, { opacity: 0, y: -80, delay: 1.1 });
+gsap.from(".home__bird-1", 1.2, { opacity: 0, y: -80, delay: 1.1 });
+// .home__img-1
+// .home__img-4
